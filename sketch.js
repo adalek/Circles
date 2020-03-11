dd2966491949f502db5c641d96e98651e07a1c3c
adalek <61150101+adalek@users.noreply.github.com>
2020-03-11 22:37:31 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-let a=200
+let a=0
 function setup(){
     createCanvas(500, 500);
 }
@@ -9,14 +9,22 @@ function draw(){
     background(0)
     stroke (200)
     noFill(0)
-    rect (0,0,100,100)
-    let x1 = map(mouseX, 0, width, 25, 75);
-    ellipse(x1, 25, 25, 25);
-    
+    let x1 = map(mouseX, 0, width, 0, 100);
+    let x3= map(mouseY, 0, width, 0, 100);
+    let x2 = map(sin(a), 0, 1, 60, 400);
     a+=0.01
-    let x2 = map(noise(a), 0, 1, 25, 75);
-     
-    ellipse(x2, 25, 25, 25);
-  
+    
   //noLoop()
+   shape4(x1,x2,x3)
 }
+ function shape4(x,l,s){
+  fill (217,20,112,150);
+ ellipse(0,x,s,l);
+ //fill (4,191,191,200);
+ ellipse(-x,0,l,s);
+ //fill(166,51,126,200);
+ ellipse(0,-x,s,l);
+ //fill (4,191,173,100);
+ ellipse(x,0,l,s);
+
+ }
