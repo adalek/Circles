68a66426084f226d7dfb97cadc54408459e843f6
adalek <61150101+adalek@users.noreply.github.com>
2020-03-21 16:00:37 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,27 +1,36 @@
 let a=0;
-
+var colors=[];
 function setup(){
     createCanvas(500, 500);
+    //create colorpicker to change fore/background color;
+    //create slider to change strokeweight;
+    colorPicker1 = createColorPicker('#000000');
+    colorPicker1.position(width,height+100);
+    colorPicker2 = createColorPicker('#eae9e9');
+    colorPicker2.position(width+100,height+100);
+    slider=createSlider(1,50,1);
+    slider.position(width+200,height+100)
 }
 
 function draw(){
   translate(250,250);
-
-    background(0)
-    stroke (200)
+    //background(0);
+    background(colorPicker1.color());
+    stroke (colorPicker2.color())
+    strokeWeight(slider.value())
     noFill(0)
     let x1 = map(mouseX, 0, width, 0, 100);
     let x3= map(mouseY, 0, height, 0, 100);
-    
+  //animation effect;  
     let e=map(noise(a), 0, 1,0,10);
     a+=0.001
     let x2 = map(sin(e), 0, 1, 60, 400);
-  //noLoop()
+
   rotate(a);
   for(let angle=0; angle<=PI*2; angle+=2*PI/8){
     rotate(angle);
     ellipse(0,x1,x3,x2);
-  }
+    }
   
     rotate (-a*2)
     
@@ -33,13 +42,8 @@ function draw(){
 
 // l&s means long,short radius
  function shape4(x,l,s){
- 
  //4 circle
  ellipse(x,x,l);
- /*ellipse(-x,-x,l);
- ellipse (-x,x,l);
- ellipse(x,-x,l);*/
- 
-
 
  }
+ 
